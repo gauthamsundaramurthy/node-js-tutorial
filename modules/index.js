f4a8f9af134cf8ac00b9f2532e683bd4cb1d88ae
Gautham Sundara Murthy <gautham.murthy@thumbsignin.com>
2020-09-22 11:32:09 +0530

    Modules

---
+++ modules/index.js
@@ -0,0 +1,5 @@
+var stuff = require ('./stuff')
+
+console.log(stuff.counterFunction(['molter','volcano','lava', 'japan']))
+console.log(stuff.adderFunction(3,4))
+console.log(stuff.pi)
---
+++ modules/stuff.js
@@ -0,0 +1,10 @@
+
+module.exports.counterFunction = function (arr) {
+    return 'There are ' + arr.length + ' items'
+}
+
+module.exports.adderFunction = function (a,b) {
+    return `The sum of two numbers is ${a+b} ` 
+}
+
+module.exports.pi = 3.14
