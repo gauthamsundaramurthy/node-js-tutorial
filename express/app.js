4bc55f815acf9ef132e1112037bb4763ae28d8fe
Gautham Sundara Murthy <gautham.murthy@thumbsignin.com>
2020-09-23 12:37:30 +0530

    Express

---
+++ express/app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/contact', function(req, res) {
+    res.sendFile(__dirname + '/contact.html')
+})
+
+app.get('/profile/:id', function(req, res) {
+    res.send('You requested to see a profile with the id of ' + req.params.id)
+})
+
+
+app.listen(3000)
