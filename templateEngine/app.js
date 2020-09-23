b62e2e8bb97f7c30abd5e965384c05a0ec86a4a5
Gautham Sundara Murthy <gautham.murthy@thumbsignin.com>
2020-09-23 15:09:59 +0530

    POST request

--- templateEngine/app.js
+++ templateEngine/app.js
@@ -1,12 +1,20 @@
 var express = require('express')
 var app = express()
+var bodyParser =  require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs')
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+app.post('/contact', urlencodedParser, function(req, res) {
+    console.log(req.body)
+    res.render('contact-success', {data: req.body})
+})
 
-app.get('/contact', function(req, res) {res.render('contact', {qs: req.query})
+app.get('/contact', function(req, res) {
+    res.render('contact', {qs: req.query})
 })
 
 app.get('/profile/:name', function(req, res) {
